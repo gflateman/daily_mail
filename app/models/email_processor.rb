1afedd2a0f0b7ad4ed431b51175d541eaf64be34
gflateman <gflateman@gmail.com>
2014-01-03 12:14:04 -0500

    token baby

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,8 +1,8 @@
 class EmailProcessor
   def self.process(email)
-    org = Organization.first #find_by_address(email.to)
+    org = Organization.find_by_address(email.to.token.to_s)
     # return unless org && org.member_emails.include?(email.from)
     # org.submissions.create! body: email.body, email: email.from
-    org.submissions.create! body: email.to.to_s, email: email.from
+    org.submissions.create! body: email.to.token.to_s, email: email.from
   end
 end
