a8a464f4098c619bf4d31550be73538e67504c0f
gflateman <gflateman@gmail.com>
2014-01-02 20:00:09 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DailyMail::Application.config.secret_key_base = '5f64f86e356f8890d6d0b30645e316169cc269f4cad9026f94d2a831fbd3ba8d9a9dd3e643a7f1dd26472aa949f130c234d13b481a1fd2927f5c64580b210625'
