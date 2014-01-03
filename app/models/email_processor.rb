858e543b42a7b120841488adf9a031c6401e55a9
gflateman <gflateman@gmail.com>
2014-01-03 15:13:45 -0500

    try two

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -3,7 +3,7 @@ class EmailProcessor
     # email.to grabs an array of the recipient address before '@' sign, ie. ['digest']
     org = Organization.first #find_by_address(email.to[:token].first.to_s)
     # return unless org && org.member_emails.include?(email.from)
-    org.submissions.create! body: email.to[:token].first.to_s, email: 'try one'
-    org.submissions.create! body: email.to[:token].to_s, email: 'try two'
+    org.submissions.create! body: email.to.first[:token].first.to_s, email: 'try one'
+    org.submissions.create! body: email.to.first[:token].to_s, email: 'try two'
   end
 end
