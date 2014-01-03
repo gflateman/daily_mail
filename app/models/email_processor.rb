1bd3921f7a2313e26548133d30685d608cbc7d82
gflateman <gflateman@gmail.com>
2014-01-02 22:42:23 -0500

    slow up

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,11 +1,5 @@
 class EmailProcessor
   def self.process(email)
-    puts '///////////////////////'
-    puts '///////////////////////'
-    puts email.to
-    puts '///////////////////////'
-    puts '///////////////////////'
-    # return unless email.to == 'digest@richardpic.com'
     Submission.create!({ body: email.body, email: email.from })
   end
 end
