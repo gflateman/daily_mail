040c9ac2e987e67637f062649e942364be48a3d2
gflateman <gflateman@gmail.com>
2014-01-03 11:38:42 -0500

    typo

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,7 +1,7 @@
 class EmailProcessor
   def self.process(email)
     org = Organization.find_by_address(email.to)
-    return unless org && org.member_emails.include? email.from
+    return unless org && org.member_emails.include?(email.from)
     org.submissions.create! body: email.body, email: email.from
   end
 end
