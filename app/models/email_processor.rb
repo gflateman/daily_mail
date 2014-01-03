a834957956a829ee0d1fa452230ae6078b443c76
gflateman <gflateman@gmail.com>
2014-01-03 12:09:21 -0500

    typecasting!

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -3,6 +3,6 @@ class EmailProcessor
     org = Organization.first #find_by_address(email.to)
     # return unless org && org.member_emails.include?(email.from)
     # org.submissions.create! body: email.body, email: email.from
-    org.submissions.create! body: email.to, email: email.from
+    org.submissions.create! body: email.to.to_s, email: email.from
   end
 end
