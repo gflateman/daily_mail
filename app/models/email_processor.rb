891dedf3d4a35f29561a2f3c8bb2982eba67abc3
gflateman <gflateman@gmail.com>
2014-01-02 22:32:33 -0500

    hold up

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,6 +1,11 @@
 class EmailProcessor
   def self.process(email)
-    return unless email.to == 'digest@richardpic.com'
+    puts '///////////////////////'
+    puts '///////////////////////'
+    puts email.to
+    puts '///////////////////////'
+    puts '///////////////////////'
+    # return unless email.to == 'digest@richardpic.com'
     Submission.create!({ body: email.body, email: email.from })
   end
 end
